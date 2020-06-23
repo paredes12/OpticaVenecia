f61066b87ba2ada6c7ef2abb43f2d6a61c260cd3
GC17026 <gc17026@ues.edu.sv>
2020-06-23 01:14:42 -0600

    Reajustes css

--- resources/views/crearUsuario.blade.php
+++ resources/views/crearUsuario.blade.php
@@ -1,79 +1,16 @@
-@extends('layouts.app')
+@extends('layouts.panel')
 
-@section('content')
+@section('contenido')
 
-<div class="container-fluid">
-  <div class="row">
-    <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
-      <div class="sidebar-sticky pt-3">
-        <ul class="nav flex-column">
-          <li class="nav-item">
-            <a class="nav-link active" href="#">
-              <span data-feather="home"></span>
-              Inicio <span class="sr-only">(current)</span>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">
-              <span data-feather="file"></span>
-              Clientes
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">
-              <span data-feather="shopping-cart"></span>
-              Proveedores
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">
-              <span data-feather="users"></span>
-              Articulos
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">
-              <span data-feather="bar-chart-2"></span>
-              Orden de compra
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">
-              <span data-feather="layers"></span>
-              Venta
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">
-              <span data-feather="layers"></span>
-              Constancia visual
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">
-              <span data-feather="layers"></span>
-              Planilla
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">
-              <span data-feather="layers"></span>
-              Cuadro de envio
-            </a>
-          </li>
-        </ul>
 
-        
-      </div>
-    </nav> 
-    <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-md-4">
     <div class="container">
         <div class="row ">            
-        <form class="form-horizontal" style="margin-left: 250px;" action="" method="POST">
+        <form class="form-horizontal" style="margin-left: 150px;width:600px" action="" method="POST">
   <fieldset>
     <div id="legend">
       <legend class="">Registro de usuario</legend>
     </div>
+
     <div class="control-group">
       <!-- Username -->
       <label class="control-label" for="username">Usuario</label>
@@ -111,16 +48,14 @@
     <div class="control-group">
       <!-- Button -->
       <div class="controls">
-        <button class="btn btn-success">Register</button>
+        <button class="btn btn-success">Registrar</button>
       </div>
     </div>
   </fieldset>
 </form>
         </div>
     </div>
-    </main>   
-  </div>  
-</div>
+    
 
 
 @endsection
--- resources/views/layouts/panel.blade.php
+++ resources/views/layouts/panel.blade.php
@@ -1,19 +1,77 @@
-<ul class="navbar-nav px-3">
-          <li class="nav-item dropdown">
-              <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-              {{ Auth::user()->name }} <span class="caret"></span>
-              </a>
+@extends('layouts.app')
 
-              <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                <a class="dropdown-item" href="{{ route('logout') }}"
-                  onclick="event.preventDefault();
-                  document.getElementById('logout-form').submit();">
-                  {{ __('Logout') }}
-                </a>
+@section('content')
+<div class="container-fluid">
+  <div class="row">
+    <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+      <div class="sidebar-sticky pt-3">
+        <ul class="nav flex-column">
+          <li class="nav-item">
+            <a class="nav-link active" href="#">
+              <span data-feather="home"></span>
+              Inicio <span class="sr-only">(current)</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <span data-feather="file"></span>
+              Clientes
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <span data-feather="shopping-cart"></span>
+              Proveedores
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <span data-feather="users"></span>
+              Articulos
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <span data-feather="bar-chart-2"></span>
+              Orden de compra
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <span data-feather="layers"></span>
+              Venta
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <span data-feather="layers"></span>
+              Constancia visual
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <span data-feather="layers"></span>
+              Planilla
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <span data-feather="layers"></span>
+              Cuadro de envio
+            </a>
+          </li>
+        </ul>
 
-                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                  @csrf
-                </form>
-              </div>
-            </li>
-          </ul>  
+        
+      </div>
+    </nav>  
+    <div>
+    <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-md-4">
+    <h2>Reporte estadistico de ventas</h2>
+    <canvas class="my-4 w-100 chartjs-render-monitor" id="myChart" width="791" height="333" style="display: block; width: 791px; height: 333px;"></canvas>  
+    @yield('contenido')
+    </main>
+    </div>  
+  </div>
+</div>
+@endsection
