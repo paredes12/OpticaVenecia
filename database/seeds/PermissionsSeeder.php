71551dadc492a537405aa73c9ffa8d649a817a31
GC17026 <GC17026@ues.edu.sv>
2020-07-14 21:25:05 -0600

    arreglando seeder

--- database/seeds/PermissionsSeeder.php
+++ database/seeds/PermissionsSeeder.php
@@ -20,6 +20,39 @@ class PermissionsSeeder extends Seeder
         array_push($permissions_array, Permission::create(['name' => 'editar usuario']));
         array_push($permissions_array, Permission::create(['name' => 'eliminar usuario']));
         array_push($permissions_array, Permission::create(['name' => 'leer usuario']));
+
+        array_push($permissions_array, Permission::create(['name' => 'crear rol']));
+        array_push($permissions_array, Permission::create(['name' => 'leer rol']));
+        array_push($permissions_array, Permission::create(['name' => 'editar rol']));
+        array_push($permissions_array, Permission::create(['name' => 'eliminar rol']));
+
+        array_push($permissions_array, Permission::create(['name' => 'eliminar permiso']));
+        array_push($permissions_array, Permission::create(['name' => 'agregar permiso']));
+
+        array_push($permissions_array, Permission::create(['name' => 'crear empleado']));
+        array_push($permissions_array, Permission::create(['name' => 'leer empleado']));
+        array_push($permissions_array, Permission::create(['name' => 'editar empleado']));
+        array_push($permissions_array, Permission::create(['name' => 'eliminar empleado']));
+
+        array_push($permissions_array, Permission::create(['name' => 'crear cliente']));
+        array_push($permissions_array, Permission::create(['name' => 'leer cliente']));
+        array_push($permissions_array, Permission::create(['name' => 'editar cliente']));
+        array_push($permissions_array, Permission::create(['name' => 'eliminar cliente']));
+
+        array_push($permissions_array, Permission::create(['name' => 'crear lugar de trabajo']));
+        array_push($permissions_array, Permission::create(['name' => 'leer lugar de trabajo']));
+        array_push($permissions_array, Permission::create(['name' => 'editar lugar de trabajo']));
+        array_push($permissions_array, Permission::create(['name' => 'eliminar lugar de trabajo']));
+
+        array_push($permissions_array, Permission::create(['name' => 'crear proveedor']));
+        array_push($permissions_array, Permission::create(['name' => 'leer proveedor']));
+        array_push($permissions_array, Permission::create(['name' => 'editar proveedor']));
+        array_push($permissions_array, Permission::create(['name' => 'eliminar proveedor']));
+
+        array_push($permissions_array, Permission::create(['name' => 'crear pais']));
+        array_push($permissions_array, Permission::create(['name' => 'leer pais']));
+        array_push($permissions_array, Permission::create(['name' => 'editar pais']));
+        array_push($permissions_array, Permission::create(['name' => 'eliminar pais']));
         //$sellerPermission = Permission::create(['name' => 'leer usuario']);
         //array_push($permissions_array, $sellerPermission);
         $superAdminRole = Role::create(['name' => 'Administrador']);
@@ -31,6 +64,7 @@ class PermissionsSeeder extends Seeder
             'name' => 'admin',
             'email' => 'admin@gmail.com',
             'password' => Hash::make('admin'),
+            'active'=> '1',
         ]);
         //assign role
         $userSuperAdmin->assignRole('Administrador'); 
